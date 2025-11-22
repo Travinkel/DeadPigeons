e49da915eba6692fdc03557482e1d1f35625b219
Travinkel <ankersoe@protonmail.com>
2025-11-22 16:41:35 +0100

    fix: align api port to 5000 for nswag ci compatibility
      - update launchSettings.json to use port 5000
      - revert nswag.json to port 5000 per adr-0012

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -3,25 +3,23 @@ import js from "@eslint/js";
 import tseslint from "typescript-eslint";
 import globals from "globals";
 
-export default tseslint.config(
-  {
-    files: ["**/*.{ts,tsx}"],
-    languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
-      globals: {
-        ...globals.browser,
-        ...globals.node,
-      },
-      parser: tseslint.parser,
+export default tseslint.config({
+  files: ["**/*.{ts,tsx}"],
+  languageOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    globals: {
+      ...globals.browser,
+      ...globals.node,
     },
-    plugins: {
-      "@typescript-eslint": tseslint.plugin,
-    },
-    rules: {
-      ...js.configs.recommended.rules,
-      "no-unused-vars": "off",
-      "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
-    },
-  }
-);
+    parser: tseslint.parser,
+  },
+  plugins: {
+    "@typescript-eslint": tseslint.plugin,
+  },
+  rules: {
+    ...js.configs.recommended.rules,
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+  },
+});
