d74cc466289218f1d2032a0ed2cbad2a0700cf51
Travinkel <ankersoe@protonmail.com>
2025-11-22 15:28:17 +0100

    fix: use typescript-aware eslint rule for unused vars
      - switch from no-unused-vars to @typescript-eslint/no-unused-vars
      - eliminates false positives on type signatures

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -3,19 +3,25 @@ import js from "@eslint/js";
 import tseslint from "typescript-eslint";
 import globals from "globals";
 
-export default tseslint.config({
-  files: ["**/*.{ts,tsx}"],
-  languageOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-    globals: {
-      ...globals.browser,
-      ...globals.node,
+export default tseslint.config(
+  {
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+      parser: tseslint.parser,
     },
-    parser: tseslint.parser,
-  },
-  rules: {
-    ...js.configs.recommended.rules,
-    "no-unused-vars": "warn",
-  },
-});
+    plugins: {
+      "@typescript-eslint": tseslint.plugin,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    },
+  }
+);
